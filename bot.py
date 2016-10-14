3b8b0b8a51e772bc8f2c4482fa9ed80445c4eb0f
Kenneth Love <kennethlove@gmail.com>
2016-10-14 11:50:07 -0700

    async version

--- bot.py
+++ bot.py
@@ -1,68 +1,81 @@
+import asyncio
 from io import BytesIO
 from operator import itemgetter
 import os
 import random
-import time
 
 import glitch
 from PIL import Image
 import requests
 import telepot
+from telepot.aio.delegate import pave_event_space, per_chat_id, create_open
 
 import secrets
 
 
-def _get_the_file(bot, file_id):
-    file_info = bot.getFile(file_id)
-    url = "https://api.telegram.org/file/bot{}/{}".format(
-        secrets.TOKEN, file_info['file_path'])
-    req = requests.get(url)
-    if req.status_code == 200:
-        im = Image.open(BytesIO(req.content))
-        filename = "glitch_images/{}.jpg".format(file_id)
-        im.save(filename, 'JPEG')
-        return filename
-    return None
+class GlitchChat(telepot.aio.helper.ChatHandler):
+    async def on_chat_message(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        self.the_file = None
+        self.glitched_file = None
+
+        if content_type == 'text':
+            await self.bot.sendMessage(chat_id, msg['text'])
+        elif content_type == 'photo':
+            biggest_file = sorted(
+                msg['photo'],
+                key=itemgetter('file_size')
+            )[-1]
+            await self._get_the_file(biggest_file['file_id'])
+            await self.glitch_the_image(self.the_file)
+            if self.the_file is not None:
+                await bot.sendPhoto(
+                    chat_id,
+                    open(self.glitched_file, 'rb')
+                )
+
+    async def _get_the_file(self, file_id):
+        file_info = await self.bot.getFile(file_id)
+        url = "https://api.telegram.org/file/bot{}/{}".format(
+            secrets.TOKEN, file_info['file_path'])
+        req = requests.get(url)
+        if req.status_code == 200:
+            im = Image.open(BytesIO(req.content))
+            filename = "glitch_images/{}.jpg".format(file_id)
+            im.save(filename, 'JPEG')
+            self.the_file = filename
+            return
+        self.the_file = None
+        return
+
+    async def glitch_the_image(self, filename):
+        glitcher = glitch.Glitch()
+        dirless_filename = os.path.split(filename)[-1]
+        extless_filename = os.path.splitext(dirless_filename)[0]
+        glitcher.glitch(
+            filename,
+            "glitch_images/{}_glitched.jpg".format(extless_filename),
+            times=5,
+            mode=random.choice("ridsc")
+        )
+
+        files = list(filter(
+            lambda f: extless_filename in f,
+            os.listdir(path='glitch_images')
+        ))
+        self.glitched_file = "glitch_images/{}".format(random.choice(files))
+        return
 
 
-def glitch_the_image(filename):
-    glitcher = glitch.Glitch()
-    dirless_filename = os.path.split(filename)[-1]
-    extless_filename = os.path.splitext(dirless_filename)[0]
-    glitcher.glitch(
-        filename,
-        "glitch_images/{}_glitched.jpg".format(extless_filename),
-        times=5,
-        mode="i"
+bot = telepot.aio.DelegatorBot(secrets.TOKEN, [
+    pave_event_space()(
+        per_chat_id(), create_open, GlitchChat, timeout=10
     )
+])
 
-    files = list(filter(
-        lambda f: extless_filename in f,
-        os.listdir(path='glitch_images')
-    ))
-    return "glitch_images/{}".format(random.choice(files))
-
-
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
-
-    if content_type == 'text':
-        bot.sendMessage(chat_id, msg['text'])
-    elif content_type == 'photo':
-        biggest_file = sorted(msg['photo'], key=itemgetter('file_size'))[-1]
-        the_file = _get_the_file(bot, biggest_file['file_id'])
-        glitched_file = glitch_the_image(the_file)
-        if the_file is not None:
-            bot.sendPhoto(
-                chat_id,
-                open(glitched_file, 'rb')
-            )
-
-bot = telepot.Bot(secrets.TOKEN)
-bot.message_loop(handle)
+loop = asyncio.get_event_loop()
+loop.create_task(bot.message_loop())
 
 print("Listening...")
 
-while 1:
-    time.sleep(10)
+loop.run_forever()
